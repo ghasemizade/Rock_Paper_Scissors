b76bbbd0f7c18e2a6257eaa11d111619b3c32ff9
Hosein <mrqasemizade@gmail.com>
2022-08-03 12:13:13 +0430

    update 0.1v

--- rock_paper_scissors/lib/main.dart
+++ rock_paper_scissors/lib/main.dart
@@ -1,56 +1,86 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(Application());
 }
 
-class Application extends StatelessWidget {
-  const Application({Key? key}) : super(key: key);
+class Application extends StatefulWidget {
+  Application({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    String top = 'paper';
-    String bottom = 'scissors';
+  State<Application> createState() => _ApplicationState();
+}
 
+class _ApplicationState extends State<Application> {
+  int top = 1;
+  int bottom = 1;
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.grey[850],
         appBar: AppBar(
           centerTitle: true,
           title: Text(
             'Rock Paper Scissors',
             style: TextStyle(color: Colors.amber),
           ),
-          backgroundColor: Colors.teal[700],
+          backgroundColor: Colors.grey[900],
         ),
         body: SafeArea(
-            child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image(
-                image: AssetImage('Images/$top.png'),
-              ),
-              SizedBox(height: 150),
-              TextButton(
-                onPressed: (() {}),
-                child: Text(
-                  'Start Game',
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'User 1',
                   style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 28.0,
-                      fontWeight: FontWeight.bold),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue),
                 ),
-              ),
-              SizedBox(height: 150),
-              Image(
-                image: AssetImage('Images/$bottom.png'),
-              ),
-            ],
+                Image(
+                  image: AssetImage('Images/$top.png'),
+                ),
+                SizedBox(height: 150),
+                TextButton(
+                  onPressed: () {
+                    setState(
+                      () {
+                        setState(() {
+                          top = Random().nextInt(3) + 1;
+                          bottom = Random().nextInt(3) + 1;
+                        });
+                      },
+                    );
+                  },
+                  child: Text(
+                    'Start Game',
+                    style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(height: 150),
+                Text(
+                  'User 2',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red),
+                ),
+                Image(
+                  image: AssetImage('Images/$bottom.png'),
+                ),
+              ],
+            ),
           ),
-        )),
+        ),
       ),
     );
   }
